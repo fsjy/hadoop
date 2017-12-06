dfb6da176654080494e9e6b46d979f3abaca98fe
Dacular <forevervip@hotmail.com>
2017-12-06 10:43:02 +0800

    add comment in WCMapper.java

--- hadoophdfs/src/main/java/com/yanglu/hadoop/mr/WCMapper.java
+++ hadoophdfs/src/main/java/com/yanglu/hadoop/mr/WCMapper.java
@@ -7,6 +7,16 @@ import org.codehaus.jackson.jaxrs.MapperConfigurator;
 
 import java.io.IOException;
 
+/**
+ * 四个范型中，前两个是指定mapper输入数据的类型，为key-value的键值对。
+ * <p></p>
+ * map和reduce的数据输入输出都是key-value对封装
+ * <p></p>
+ * 默认情况下，框架传递给我们的mapper的输入数据中，key是要处理的文本中一行的其实偏移量，这一行的内容作为value传递进来
+ *
+ * @see org.apache.hadoop.mapreduce.Mapper
+ * @author Dacular
+ */
 public class WCMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
 
 
@@ -21,6 +31,7 @@ public class WCMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
      */
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
 
 
     }
