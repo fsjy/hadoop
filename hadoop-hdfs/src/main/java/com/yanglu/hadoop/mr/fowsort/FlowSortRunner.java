f7927fc0905c9a421f5e114375a7622c869b4943
Dacular <forevervip@hotmail.com>
2017-12-07 19:42:25 +0800

    temp

---
+++ hadoop-hdfs/src/main/java/com/yanglu/hadoop/mr/fowsort/FlowSortRunner.java
@@ -0,0 +1,44 @@
+package com.yanglu.hadoop.mr.fowsort;
+
+import com.yanglu.hadoop.mr.flow.FlowRunner;
+import com.yanglu.hadoop.mr.flow.untity.FlowBean;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+public class FlowSortRunner {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+
+        Configuration configuration = new Configuration();
+
+        // configuration.set("fs.defaultFS", "hdfs://centos01:9000");
+        configuration.set("fs.defaultFS", "hdfs://centos01:9000");
+        configuration.set("mapreduce.job.jar", "/Users/yl/.m2/repository/com/yanglu/hadoop-hdfs/1.0-SNAPSHOT/hadoop-hdfs-1.0-SNAPSHOT.jar");
+
+
+        Job job = Job.getInstance(configuration);
+
+        job.setJarByClass(FlowSortRunner.class);
+
+        job.setMapperClass(FlowSortMR.SortMapper.class);
+        job.setReducerClass(FlowSortMR.SortReducer.class);
+
+        job.setMapOutputKeyClass(FlowBean.class);
+        job.setMapOutputValueClass(NullWritable.class);
+
+        job.setOutputKeyClass(FlowBean.class);
+        job.setOutputValueClass(NullWritable.class);
+
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        System.exit(job.waitForCompletion(true) ? 1 : 0);
+
+    }
+}
